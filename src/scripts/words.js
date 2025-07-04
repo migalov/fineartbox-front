b2dbb5ac8dec719662f9c9225e5a491f215ec087
Michel Muniagiseny <mafri.michel@gmail.com>
2025-07-04 19:00:39 +0300

    Move to github and update package page

---
+++ src/scripts/words.js
@@ -0,0 +1,13 @@
+
+
+function replaceSpacesAfterPrepositions(text) {
+    const prepositions = ['в', 'на', 'с', 'к', 'по', 'из', 'от', 'для']; // Добавьте нужные предлоги
+    const conjunctions = ['и', 'да', 'или', 'но']; // Добавьте нужные союзы
+    const regex = new RegExp(`(${prepositions.join('|')})\\s+|(${conjunctions.join('|')})\\s+`, 'g');
+    
+    return text.replace(regex, (match, p1, p2) => {
+        return (p1 ? p1 + '\u00A0' : p2 + '\u00A0');
+    });
+}
+
+export default replaceSpacesAfterPrepositions;
