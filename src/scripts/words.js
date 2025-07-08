7ef5db17be63a2e3ee1c6037b644cd683fdcb700
Michel Muniagiseny <mafri.michel@gmail.com>
2025-07-08 16:12:08 +0300

    Global fixing css

--- src/scripts/words.js
+++ src/scripts/words.js
@@ -1,7 +1,7 @@
 
 
 function replaceSpacesAfterPrepositions(text) {
-    const prepositions = ['в', 'на', 'с', 'к', 'по', 'из', 'от', 'для']; // Добавьте нужные предлоги
+    const prepositions = ['в', 'на', 'с', 'к', 'по', 'из', 'от', 'для', 'под']; // Добавьте нужные предлоги
     const conjunctions = ['и', 'да', 'или', 'но']; // Добавьте нужные союзы
     const regex = new RegExp(`(${prepositions.join('|')})\\s+|(${conjunctions.join('|')})\\s+`, 'g');
     
