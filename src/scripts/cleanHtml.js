f5f32c86ce7f63969039e507aac179ee6a76e38e
Michel Muniagiseny <mafri.michel@gmail.com>
2025-08-01 18:58:16 +0300

    Finished finish page

--- src/scripts/cleanHtml.js
+++ src/scripts/cleanHtml.js
@@ -1,6 +1,6 @@
 
 import sanitizeHtml from 'sanitize-html';
-const cleanHtml = (rawHtml: any) => sanitizeHtml(rawHtml, {
+const cleanHtml = (rawHtml) => sanitizeHtml(rawHtml, {
        allowedTags: ['p', 'h1', 'h2' ,'br', 'strong', 'ol', 'ul', 'li', 'br', 'table', 'tbody', 'tr', 'td'], // Разрешенные теги
        allowedAttributes: {
         'a': ['href']
